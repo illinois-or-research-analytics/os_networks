2fb08716de26f1a8e5ffe45dcacb81abafc782d3
George Chacko <chackoge@illinois.edu>
2023-11-08 15:39:32 -0600

    generic update

--- parse_pmd.R
+++ parse_pmd.R
@@ -1,6 +1,5 @@
 # parses a compressed Pubmed XML file and returns a two column data frame.
 # uses the syntax from https://rsangole.netlify.app/post/try-catch/
-
 library(xml2)
 library(data.table)
 library(feather)
@@ -24,19 +23,21 @@ parse_pmd <- function(x) {
 		message("All done")
 	})
 }
-
 df_list <- list()
 setwd('/shared/pubmed_copy')
 file_list <- list.files(pattern="*.xml.gz")
 
 for (i in 1:1000) {
 df_list[[i]] <- parse_pmd(file_list[i])
+}
+
 pmid_doi_df <- rbindlist(df_list)
 pmid_doi_df$pmid <- as.integer(pmid_doi_df$pmid)
 
 setwd('/shared/pubmed')
-fwrite(pmid_doi_df,file='pmid_doi.csv')
-write_feather(pmid_doi_df,'pmid_doi.feather')
-}
+fwrite(pmid_doi_df1,file='pmid_doi.csv')
+write_feather(pmid_doi_df1,'pmid_doi.feather')
+
+
 
 
