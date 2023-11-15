c24a05e990585f728b08a3c034476cbe5b32f19f
George Chacko <chackoge@illinois.edu>
2023-11-15 13:02:48 -0500

    generic update

--- parse_pubmed.sql
+++ parse_pubmed.sql
@@ -2,28 +2,31 @@
 -- load pmid_doi.csv into the public.pmid_doi_parsed table
 
 -- remove null PMID rows
-SELECT COUNT(1) from public.pmid_doi_parsed;
-DELETE from public.pmid_doi_parsed WHERE pmid IS NULL;
+SELECT COUNT(1) from public.pmid_doi_parsed; --36416016
+DELETE from public.pmid_doi_parsed WHERE pmid IS NULL; --36416015
 
---eliminate all pmids where doi is null or empty string
-DELETE FROM public.pmid_doi_parsed where doi is null;
-DELETE FROM public.pmid_doi_parsed where doi = '';
+SELECT COUNT(1) FROM pmid_doi_parsed where doi_eloc is null; --20012712
+SELECT COUNT(1) FROM pmid_doi_parsed where doi_articleid is null; --8884053
+SELECT COUNT(1) FROM pmid_doi_parsed where doi_articleid != doi_eloc AND doi_eloc is not null; --15566
+
+--eliminate all pmids where doi_articleid is null or empty string
+DELETE FROM public.pmid_doi_parsed where doi_articleid is null;
+DELETE FROM public.pmid_doi_parsed where doi_articleid = '';
 
 -- force lower case on all dois
-UPDATE public.pmid_doi_parsed SET doi = lower(doi);
+UPDATE public.pmid_doi_parsed SET doi_articleid = lower(doi_articleid);
 
 -- create index
 DROP INDEX IF EXISTS pmid_doi_parsed_idx;
-CREATE INDEX pmid_doi_parsed_idx on public.pmid_doi_parsed(pmid,doi);
+CREATE INDEX pmid_doi_parsed_idx on public.pmid_doi_parsed(pmid,doi_articleid);
 
--- export to CSV and use unique in R to easily suppress duplicates.
--- truncate table and reload de-duplicated.
-
--- check for duplicate pmids, dois, etc. and delete
 DELETE FROM pmid_doi_parsed WHERE PMID IN
-(SELECT pmid FROM pmid_doi_parsed GROUP BY pmid HAVING COUNT(doi) > 1);
+(SELECT pmid FROM pmid_doi_parsed GROUP BY pmid HAVING COUNT(doi_articleid) > 1);
 
-DELETE FROM pmid_doi_parsed WHERE doi IN
-(SELECT doi FROM pmid_doi_parsed GROUP BY doi HAVING COUNT(pmid) > 1);
+DELETE FROM pmid_doi_parsed WHERE doi_articleid IN
+(SELECT doi_articleid FROM pmid_doi_parsed GROUP BY doi_articleid HAVING COUNT(pmid) > 1);
 
 SELECT COUNT(1) from public.pmid_doi_parsed;
+
+-- export cleaned and de-duplicated data to csv
+COPY (SELECT * FROM public.pmid_doi_parsed) TO '/tmp/pmid_doi_cleaned.csv' WITH(FORMAT CSV, HEADER);
--- pubmed_restricted_pi3k.sql
+++ pubmed_restricted_pi3k.sql
@@ -1,14 +1,16 @@
 select count(1) from public.pi3k_nl;
 
+DROP TABLE IF EXISTS public.pi3k_pubmed_restricted_nl;
 CREATE TABLE public.pi3k_pubmed_restricted_nl AS
 SELECT pn.iid,pdp.pmid,ocp.doi from public.pi3k_nl pn
 INNER JOIN public.open_citation_pubs ocp
 ON ocp.iid=pn.iid
 INNER JOIN public.pmid_doi_parsed pdp
-ON pdp.doi=ocp.doi;
+ON pdp.doi_articleid=ocp.doi;
 
 SELECT COUNT(1) FROM pi3k_pubmed_restricted_nl;
 
+DROP TABLE IF EXISTS pi3k_pubmed_restricted_el;
 CREATE TABLE pi3k_pubmed_restricted_el AS
 SELECT pe.citing_iid, pe.cited_iid FROM pi3k_el pe
 INNER JOIN public.pi3k_pubmed_restricted_nl pn1
